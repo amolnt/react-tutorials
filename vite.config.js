971908eabaa7fff6a8ca84042086dea3f4e1f8fc
amolnt <tribhuwan131@gmail.com>
2025-01-27 23:51:24 +0530

    Allowd all hosts

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all IP addresses
+    allowedHosts: true, // Allow all hosts
+  },
   test :{
     globals : true,
     environment : "jsdom",
